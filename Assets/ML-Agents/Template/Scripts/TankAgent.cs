96d2539589326df76a087080f39b96bd801c027d
Cody <codyethanjordan@users.noreply.github.com>
2017-12-30 22:57:34 -0500

    Making brains and agents

---
+++ Assets/ML-Agents/Template/Scripts/TankAgent.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankAgent : Agent
+{
+
+    private GameObject destination;
+
+    public override void InitializeAgent()
+    {
+        base.InitializeAgent();
+        destination = GameObject.FindGameObjectWithTag("Finish");
+    }
+
+    public override List<float> CollectState()
+    {
+        List<float> state = new List<float>();
+        state.Add(transform.position.x);
+        state.Add(transform.position.y);
+        state.Add(destination.transform.position.x);
+        state.Add(destination.transform.position.y);
+
+        return state;
+    }
+
+    public override void AgentStep(float[] act)
+    {
+        if (act.Length > 0)
+        {
+            Debug.Log(act.Length + " " + act[0]);
+        }
+    }
+
+    public override void AgentReset()
+    {
+        transform.position = Vector3.zero;
+    }
+
+    public override void AgentOnDone()
+    {
+
+    }
+}
