0a8d470bc87dcb403018f85320ae528207c113c5
CodyEthanJordan <codyethanjordan@users.noreply.github.com>
2018-01-14 23:34:31 -0500

    Adding turret

---
+++ Assets/Scripts/AI/TurretController.cs
@@ -0,0 +1,53 @@
+﻿using Assets.Scripts.Stats;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+namespace Assets.Scripts.AI
+{
+    class TurretController : Unit
+    {
+        private SpriteRenderer sr;
+        private Rigidbody2D rb;
+
+
+        public TurretStats stats;
+
+
+        private void Awake()
+        {
+            sr = GetComponent<SpriteRenderer>();
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        private void Start()
+        {
+            Health = stats.MaxHealth;
+        }
+
+        public override void TakeDamage(float damage)
+        {
+            if (!isServer)
+            {
+                return;
+            }
+
+            Health -= damage;
+
+            if (Health <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        protected override void OnChangedHealth(float currentHealth)
+        {
+            sr.color = new Color(1, currentHealth / MaxHealth, currentHealth / MaxHealth);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Stats/TurretStats.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+namespace Assets.Scripts.Stats
+{
+    [CreateAssetMenu(menuName = "parhelion/EnemyStats/TurretType")]
+    class TurretStats : EnemyStats
+    {
+        public float ChaseThreshold;
+        public float WanderTime;
+        public float GiveUpDistanceThreshold;
+        public float CollisionDamage;
+        public float CollisionSelfDamage;
+    }
+}
